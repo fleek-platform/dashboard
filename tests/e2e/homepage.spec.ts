6d2d8afd517f32ae9867ca6beb7c4462bd7224bd
Helder Oliveira <heldrida@gmail.com>
2024-11-14 11:16:58 +0000

    test: 💍 tweak homepage tests

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,23 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test as it, expect } from '@playwright/test';
 
-const { describe } = test;
+const { describe, beforeEach } = it;
 
 describe('On Home page', () => {
   describe('A non-authenticated user', () => {
-    test.beforeEach(async ({ page }) => {
+    beforeEach(async ({ page }) => {
       await page.goto(`http://localhost:${process.env.NEXT_DEV_SERVER_PORT}`);
     });
 
-    test('Should get the title page "Home"', async ({ page }) => {
+    it('Should get the title page "Home"', async ({ page }) => {
       await expect(page).toHaveTitle(/Home - Fleek/);
     });
 
-    test('Should have login or signup modal', async ({ page }) => {
+    it('Should have login or signup modal', async ({ page }) => {
       const el = page.locator('.modal');
       await expect(el).toContainText('Log in or sign up');
     });
 
-    test('Should have a sign-in header button', async ({ page }) => {
+    it('Should have a sign-in header button', async ({ page }) => {
       const el = page.locator('header');
       await expect(el).toContainText('Sign in');
     });
