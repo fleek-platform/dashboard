70fde8f62c9206fcf85c4a6afa29ad95713c9e2d
Victor F. Santos <victor.gaard@gmail.com>
2024-12-04 14:42:19 -0300

    chore: port tailwind config files (#7)
    ## Why?
    - Porting unsynced tailwind config files.
    
    ## How?
    
    - Done A (replace with a breakdown of the steps)
    - Done B
    - Done C
    
    ## Tickets?
    
    - [Ticket 1](the-ticket-url-here)
    - [Ticket 2](the-ticket-url-here)
    - [Ticket 3](the-ticket-url-here)
    
    ## Contribution checklist?
    
    - [ ] The commit messages are detailed
    - [ ] The `build` command runs locally
    - [ ] Assets or static content are linked and stored in the project
    - [ ] You've reviewed spelling using a grammar checker
    - [ ] For documentation, guides or references, you've tested the
    commands and steps
    - [ ] You've done enough research before writing
    
    ## Security checklist?
    
    - [ ] Sensitive data has been identified and is being protected properly
    - [ ] Injection has been prevented (parameterized queries, no eval or
    system calls)
    - [ ] The Components are escaping output (to prevent XSS)
    
    ## References?
    
    Optionally, provide references such as links
    
    ## Preview?
    
    Optionally, provide the preview url here

---
+++ .tailwind/borderRadius.ts
@@ -0,0 +1,11 @@
+export const borderRadius = {
+  DEFAULT: '0.5rem',
+  none: '0px',
+  min: '1px',
+  sm: '0.25rem',
+  md: '0.5rem',
+  lg: '0.75rem',
+  xl: '1rem',
+  '3xl': '22.5rem',
+  full: '9999px',
+};
--- .tailwind/index.ts
+++ .tailwind/index.ts
@@ -2,3 +2,4 @@ export * from './colors';
 export * from './spacing';
 export * from './fontSize';
 export * from './extend';
+export * from './borderRadius';
